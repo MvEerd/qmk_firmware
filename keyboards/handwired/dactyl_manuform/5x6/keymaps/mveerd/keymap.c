234cef4fe49eb9acac425e068519b88d3746d213
Michael van Eerd <michael@cache.works>
2021-06-22 17:42:59 +0200

    f

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/mveerd/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/mveerd/keymap.c
@@ -26,7 +26,7 @@
 #define RGB_REV     RGB_MODE_REVERSE
 #define ALTRIGHT    LALT_T(KC_RIGHT)
 #define SFT_QUOTE   MT(MOD_RSFT, KC_QUOTE)
-#define SFT_TAB     MT(KC_TAB, KC_QUOTE)
+#define SFT_TAB     MT(KC_TAB, MOD_RSFT)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
