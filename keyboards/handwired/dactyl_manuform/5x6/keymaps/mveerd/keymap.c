61fb1bd5e391be82af63631f17095fc45d8ca301
Michael van Eerd <michael@cache.works>
2021-06-22 17:42:02 +0200

    space/backspace/control

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/mveerd/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/mveerd/keymap.c
@@ -31,14 +31,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [_QWERTY] = LAYOUT_5x6(
-     KC_EQL    ,KC_1      ,KC_2      ,KC_3      ,KC_4      ,KC_5      ,                KC_6      ,KC_7      ,KC_8      ,KC_9      ,KC_0      ,KC_MINS   ,
-     KC_ESC    ,KC_Q      ,KC_W      ,KC_E      ,KC_R      ,KC_T      ,                KC_Y      ,KC_U      ,KC_I      ,KC_O      ,KC_P      ,KC_BSLASH ,
-     KC_LSFT   ,KC_A      ,KC_S      ,KC_D      ,KC_F      ,KC_G      ,                KC_H      ,KC_J      ,KC_K      ,KC_L      ,L2_SCLN   ,SFT_QUOTE ,
-     L1_GRV    ,KC_Z      ,KC_X      ,KC_C      ,KC_V      ,KC_B      ,                KC_N      ,KC_M      ,KC_COMM   ,KC_DOT    ,KC_SLSH   ,L3_BSLASH ,
-                           KC_LEFT   ,ALTRIGHT  ,                                                            KC_UP     ,KC_DOWN   ,
-                                                 CTL_SPACE ,L1_BSPACE ,                SFT_TAB   ,CTL_ENTER ,
-                                                 KC_PGUP   ,KC_F24    ,                KC_END    ,KC_DEL    ,
-                                                 KC_PGDN   ,KC_LGUI   ,                KC_LGUI   ,KC_LALT
+     KC_EQL    ,KC_1      ,KC_2      ,KC_3      ,KC_4      ,KC_5       ,                KC_6      ,KC_7      ,KC_8      ,KC_9      ,KC_0      ,KC_MINS   ,
+     KC_ESC    ,KC_Q      ,KC_W      ,KC_E      ,KC_R      ,KC_T       ,                KC_Y      ,KC_U      ,KC_I      ,KC_O      ,KC_P      ,KC_BSLASH ,
+     KC_LSFT   ,KC_A      ,KC_S      ,KC_D      ,KC_F      ,KC_G       ,                KC_H      ,KC_J      ,KC_K      ,KC_L      ,L2_SCLN   ,SFT_QUOTE ,
+     L1_GRV    ,KC_Z      ,KC_X      ,KC_C      ,KC_V      ,KC_B       ,                KC_N      ,KC_M      ,KC_COMM   ,KC_DOT    ,KC_SLSH   ,L3_BSLASH ,
+                           KC_LEFT   ,ALTRIGHT  ,                                                             KC_UP     ,KC_DOWN   ,
+                                                 LT(L1,KC_SPACE) ,CTL_BSPACE ,                SFT_TAB   ,CTL_ENTER ,
+                                                 KC_PGUP   ,KC_F24     ,                KC_END    ,KC_DEL    ,
+                                                 KC_PGDN   ,KC_LGUI    ,                KC_LGUI   ,KC_LALT
   ),
     
    [_COLE] = LAYOUT_5x6(
